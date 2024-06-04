f349bd6b844a6a731dbe9fb345f45f2203c9947a
Gersitz <125976146+Gersitz@users.noreply.github.com>
2024-06-05 00:01:21 +0200

    improved physics, added first wallslide and walljump

--- README.md
+++ README.md
@@ -3,6 +3,21 @@
 ## What is this?
 This is my first attempt to make my own 2D game using the Godot engine. So far, there is not much to see, but I'll keep this repository updated as I get further in development.
 
+## What is this game about?
+This game will eventually become a platformer with multiple different levels. Every level will be a single screen that consists of platforming and solving small puzzles. The level will be cleared once you collected every item and you will get a rating depending on how fast you were able to clear the level.
+
+## What is planned?
+To start, I want to create about 10 different levels. Once those are done, I have multiple different things I want to do:
+
+- Create a Title Screen
+- Create a Level Select
+- Create a Settings Menu
+- Add different characters
+- Make different characters have slightly different movement behavior
+- Make separate levels for each character
+
+... and more.
+
 ## License
 This project is licensed under the [Creative Commons Attribution-NonCommercial-NoDerivs 4.0 International](https://creativecommons.org/licenses/by-nc-nd/4.0/) License. You are free to download and share the work for personal, non-commercial purposes, but you cannot modify the work in any way or use it for commercial purposes.
 
--- V-Boy.gd
+++ V-Boy.gd
@@ -1,26 +1,27 @@
 extends CharacterBody2D
 
+@export var move_speed = 220
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -500.0
+@export var jump_height = 100
+@export var jump_time_to_peak = 0.40
+@export var jump_time_to_descent = 0.35
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var jump_velocity = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+@onready var jump_gravity = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+@onready var fall_gravity = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
+
+@onready var walljump_gravity = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent * -5.0)) * 1-0
+const wall_jump_pushback = 1000
+const wall_slide_speed = 1
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var ray_cast_right = $RayCast2D_right
+@onready var ray_cast_left = $RayCast2D_left
 
+var double_jump_buff = false
+var jump_counter = 1
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
 	
 	if direction > 0:
@@ -28,17 +29,75 @@ func _physics_process(delta):
 	elif direction < 0:
 		animated_sprite.flip_h = true
 	
+	velocity.y += get_gravity() * delta
+	velocity.x = get_input_velocity() * move_speed
+	
 	if is_on_floor():
+		jump_counter = 1
 		if direction == 0:
 			animated_sprite.play("idle")
 		else:
 			animated_sprite.play("run")
-	else:
-		animated_sprite.play("jump")
 		
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	if not is_on_floor():
+		if ray_cast_right.is_colliding() or ray_cast_left.is_colliding():
+			animated_sprite.play("wall_jump")
+			if velocity.y > 0:
+				get_gravity()
+			if Input.is_action_just_pressed("jump"):
+				wall_jump()
+		if not (ray_cast_right.is_colliding() or ray_cast_left.is_colliding()) and velocity.y > 0:
+			animated_sprite.play("fall")
+		elif not (ray_cast_right.is_colliding() or ray_cast_left.is_colliding()) and velocity.y < 0:
+			animated_sprite.play("jump")
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		animated_sprite.play("jump")
+		jump()
+		
+	if Input.is_action_just_pressed("jump") and not is_on_floor():
+		if jump_counter < 1:
+			jump_counter += 1
+			double_jump()
+		
 	move_and_slide()
+	
+func get_gravity():
+	if (ray_cast_right.is_colliding() or ray_cast_left.is_colliding()) and velocity.y > 0:
+		return walljump_gravity
+	else:
+		return jump_gravity if velocity.y < 0.0 else fall_gravity
+	
+func jump():
+	velocity.y = jump_velocity
+	
+func double_jump():
+	velocity.y = jump_velocity * 0.8
+	
+func wall_jump():
+	if Input.is_action_pressed("move_right"):
+		velocity.x = -wall_jump_pushback
+		velocity.y = jump_velocity * 0.6
+	elif Input.is_action_pressed("move_left"):
+		velocity.x = wall_jump_pushback
+		velocity.y = jump_velocity * 0.6
+	var acceleration = 100
+	var target_velocity = Vector2.ZERO
+	
+	var acceleration_vector = (target_velocity - velocity).normalized() * acceleration * get_process_delta_time()
+	velocity += acceleration_vector
+	
+	velocity.x = clamp(velocity.x, -wall_jump_pushback, wall_jump_pushback)
+	
+	move_and_slide()
+		
+	
+func get_input_velocity():
+	var horizontal = 0.0
+	
+	if Input.is_action_pressed("move_left"):
+		horizontal = -1.0
+	if Input.is_action_pressed("move_right"):
+		horizontal = 1.0
+		
+	return horizontal
